4032dadbe6c22d9f2b5ad6d2df2e6c5af9ce5598
Сергей Ковальский <237264@mail.ru>
2023-03-22 09:42:18 +0300

    Созданы модели

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "orders.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "diplom_db",
+        "USER": "sergey",
+        "PASSWORD": "1988",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -114,7 +118,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
